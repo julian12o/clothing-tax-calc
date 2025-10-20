c7e261a51c0bbd4907f733ee059a1bd6bf9b4893
julian12o <julian.noahortega@icloud.com>
2025-10-20 09:19:06 -0400

    Update clothingtax.py

--- clothingtax.py
+++ clothingtax.py
@@ -1,3 +1,4 @@
 price = input("Enter the price of the most recent clothing item you purchased (singular): ")
 brand = input("Enter the brand of the most recent clothing item you purchased: ")
+residence = input("Enter the location you bought or where you delivered it if you bought it online: ")
 amountofclothing = input("Enter the amount of the most recent clothing item you purchased: ")
