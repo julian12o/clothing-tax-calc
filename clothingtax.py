81d807eaf51e622e9e9be50d0f1ddbd333a253b6
julian12o <julian.noahortega@icloud.com>
2025-10-20 09:17:09 -0400

    Add files via upload

---
+++ clothingtax.py
@@ -0,0 +1,3 @@
+price = input("Enter the price of the most recent clothing item you purchased (singular): ")
+brand = input("Enter the brand of the most recent clothing item you purchased: ")
+amountofclothing = input("Enter the amount of the most recent clothing item you purchased: ")
