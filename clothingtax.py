d8b939a340a67f13161253c418f8037192d70d82
julian12o <julian.noahortega@icloud.com>
2025-10-23 09:11:33 -0400

    Update clothingtax.py

--- clothingtax.py
+++ clothingtax.py
@@ -1,4 +1,8 @@
-price = input("Enter the price of the most recent clothing item you purchased (singular): ")
+price = input(float("Enter the price of the most recent clothing item you purchased (singular item, hundrenth place: "))
 brand = input("Enter the brand of the most recent clothing item you purchased: ")
 residence = input("Enter the location you bought or where you delivered it if you bought it online: ")
-amountofclothing = input("Enter the amount of the most recent clothing item you purchased: ")
+amountofclothing = input(int("Enter the amount of the most recent clothing item you purchased: "))
+#Clothes with Tax
+pta = price * amountofclothing 
+awptt =  awp * 106.625
+print ("the amount you are going to pay with tax is a: ", awptt, "the brand you purchased it from was ", brand, " and finally the location in which you bought it was ", residence)
